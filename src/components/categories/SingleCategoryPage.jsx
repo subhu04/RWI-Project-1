34842dc1909a4d6db7ac4a016f4c0ec91a40afe1
Saurabh Singh <saurabh2904singh@gmail.com>
2024-05-03 13:36:41 +0530

    update

--- src/components/categories/SingleCategoryPage.jsx
+++ src/components/categories/SingleCategoryPage.jsx
@@ -288,7 +288,249 @@ const SingleCategoryPage = () => {
       price: "$399",
       name: "Asus ROG Phone 5",
       category: "mobile"
+    },
+{
+  src:'https://m.media-amazon.com/images/I/81T-W+2GShL._AC_UY1100_.jpg',
+  alt:'Product 1',
+  price:'$66',
+  name: 'Fedora Hat',
+  category:'fashion'
+},
+{
+  src:'https://rukminim2.flixcart.com/image/850/1000/jt1tq4w0/hat/f/x/a/solid-fedora-cap-free-1-hat-a-hat-2946-tinytales-original-imafefhttbjxccfc.jpeg?q=20&crop=false',
+  alt:'Product 1',
+  price:'$16',
+  name: 'TinyTales Round Hat Cap',
+  category:'fashion'
+},
+{
+  src:'https://m.media-amazon.com/images/I/616JpRllmNL._AC_UF1000,1000_QL80_.jpg',
+  alt:'Product 1',
+  price:'$5',
+  name: 'Moraze Glossy Touch Nude Nail Polish',
+  category:'fashion'
+},
+{
+  src:'https://rukminim2.flixcart.com/image/850/1000/xif0q/nail-polish/o/x/9/19-matte-finish-glitter-gel-nail-paint-for-party-wear-look-original-imagk7m9dwsnfcgz.jpeg?q=20&crop=false',
+  alt:'Product 1',
+  price:'$6',
+  name: 'MYEONG MATTE FINISH & GLITTER GEL NAIL PAINT',
+  category:'fashion'
+},
+
+{
+  src: "https://images.unsplash.com/photo-1610792516775-01de03eae630?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  alt: "Product 1",
+  price: "$799",
+  name: "Samsung Galaxy S21 Ultra",
+  category: "mobile"
+},
+{
+  src: "https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?q=80&w=1567&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  alt: "Product 2",
+  price: "$999",
+  name: "iPhone 13 Pro Max",
+  category: "mobile"
+},
+{
+  src: "https://images.unsplash.com/photo-1657797066015-311cb505b009?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  alt: "Product 3",
+  price: "$899",
+  name: "Google Pixel 6 Pro",
+  category: "mobile"
+},
+{
+  src: "https://images.unsplash.com/photo-1658321466969-1c45400bb947?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  alt: "Product 4",
+  price: "$699",
+  name: "OnePlus 9 Pro",
+  category: "mobile"
+},
+{
+  src: "https://images.unsplash.com/photo-1621330396173-e41b1cafd17f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  alt: "Product 5",
+  price: "$749",
+  name: "Xiaomi Mi 11",
+  category: "mobile"
+},
+{
+  src: "https://images.unsplash.com/photo-1591884539376-42f1f0dc65c2?q=80&w=1485&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  alt: "Product 6",
+  price: "$649",
+  name: "Huawei P50 Pro",
+  category: "mobile"
+},
+{
+  src: "https://www.mobiles4sale.com/pictures/default/lg-v60-thinq-5g-dual-screen/lg-v60-thinq-5g-dual-screen-pic-1.jpg",
+  alt: "Product 7",
+  price: "$579",
+  name: "LG V60 ThinQ",
+  category: "mobile"
+},
+{
+  src: "https://www.androidplanet.nl/wp-content/uploads/2021/09/sony-xperia-1-iii-review3.jpg",
+  alt: "Product 8",
+  price: "$499",
+  name: "Sony Xperia 1 III",
+  category: "mobile"
+},
+{
+  src: "https://motorolaroe.vtexassets.com/arquivos/ids/156575/motorola-edge-20-pro-pdp-render-8-Baltic-blue-gq89b9ir.png?v=637630881043330000",
+  alt: "Product 9",
+  price: "$479",
+  name: "Motorola Edge 20",
+  category: "mobile"
+},
+{
+  src: "https://dlcdnwebimgs.asus.com/gain/D551F7D4-8AC9-4299-A93D-3860DFC9D573/w250",
+  alt: "Product 10",
+  price: "$399",
+  name: "Asus ROG Phone 5",
+  category: "mobile"
+    },
+
+  {
+      src: "https://images.unsplash.com/photo-1610792516775-01de03eae630?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 1",
+      price: "$799",
+      name: "Samsung Galaxy S21 Ultra",
+      category: "mobile"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?q=80&w=1567&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 2",
+      price: "$999",
+      name: "iPhone 13 Pro Max",
+      category: "mobile"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1657797066015-311cb505b009?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 3",
+      price: "$899",
+      name: "Google Pixel 6 Pro",
+      category: "mobile"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1658321466969-1c45400bb947?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 4",
+      price: "$699",
+      name: "OnePlus 9 Pro",
+      category: "mobile"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1621330396173-e41b1cafd17f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 5",
+      price: "$749",
+      name: "Xiaomi Mi 11",
+      category: "mobile"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1591884539376-42f1f0dc65c2?q=80&w=1485&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 6",
+      price: "$649",
+      name: "Huawei P50 Pro",
+      category: "mobile"
+  },
+  {
+      src: "https://www.mobiles4sale.com/pictures/default/lg-v60-thinq-5g-dual-screen/lg-v60-thinq-5g-dual-screen-pic-1.jpg",
+      alt: "Product 7",
+      price: "$579",
+      name: "LG V60 ThinQ",
+      category: "mobile"
+  },
+  {
+      src: "https://www.androidplanet.nl/wp-content/uploads/2021/09/sony-xperia-1-iii-review3.jpg",
+      alt: "Product 8",
+      price: "$499",
+      name: "Sony Xperia 1 III",
+      category: "mobile"
+  },
+  {
+      src: "https://motorolaroe.vtexassets.com/arquivos/ids/156575/motorola-edge-20-pro-pdp-render-8-Baltic-blue-gq89b9ir.png?v=637630881043330000",
+      alt: "Product 9",
+      price: "$479",
+      name: "Motorola Edge 20",
+      category: "mobile"
+  },
+  {
+      src: "https://dlcdnwebimgs.asus.com/gain/D551F7D4-8AC9-4299-A93D-3860DFC9D573/w250",
+      alt: "Product 10",
+      price: "$399",
+      name: "Asus ROG Phone 5",
+      category: "mobile"
+    },
+
+  {
+      src: "https://images.unsplash.com/photo-1650320079970-b4ee8f0dae33?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 1",
+      price: "$99",
+      name: "Casual Sneakers",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1562183241-b937e95585b6?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 2",
+      price: "$129",
+      name: "Running Shoes",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1547941126-3d5322b218b0?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 3",
+      price: "$79",
+      name: "Athletic Shoes",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1627631457335-d1041fd0cb8c?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 4",
+      price: "$149",
+      name: "High Heels",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1603487742131-4160ec999306?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 5",
+      price: "$89",
+      name: "Sandals",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1613673720017-56e42d90fee4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 6",
+      price: "$109",
+      name: "Leather Boots",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1509472290917-08d8d47c5fca?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 7",
+      price: "$79",
+      name: "Canvas Sneakers",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1621019845162-874621755aa8?q=80&w=1373&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 8",
+      price: "$129",
+      name: "Hiking Shoes",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1488704906310-183eeda75d51?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 9",
+      price: "$99",
+      name: "Platform Shoes",
+      category: "shoes"
+  },
+  {
+      src: "https://images.unsplash.com/photo-1604136172384-b2e9c43271ec?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      alt: "Product 10",
+      price: "$69",
+      name: "Slip-on Shoes",
+      category: "shoes"
     }
+
 
 
 
